c23c4de9546cc1d69e49aaffbdf577eb148720b3
DyeKuu <kunhao.zheng@polytechnique.edu>
2020-05-19 10:27:26 +0200

    update 1431

---
+++ LC1431kidsWithTheGreatestNumberOfCandies.cpp
@@ -0,0 +1,15 @@
+#include <algorithm>
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        int maxValue = *std::max_element(candies.begin(),candies.end());
+        vector<bool> result;
+        for (int i = 0; i < candies.size(); i++){
+            if (candies[i] + extraCandies >= maxValue)
+                result.push_back(true);
+            else
+                result.push_back(false);
+        }
+        return result;
+    }
+};
