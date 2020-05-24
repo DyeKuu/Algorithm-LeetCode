bc0ee37a392604da2010fa0b5e822e8c3fb2a612
DyeKuu <kunhao.zheng@polytechnique.edu>
2020-05-25 00:14:12 +0200

    update 746

---
+++ LC746minCostClimbingStairs.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n = cost.size();
+        int F[n+1];
+        F[0] = cost[0], F[1] = cost[1];
+        for (int i = 2; i < n; i++){
+            F[i] = min(F[i-1],F[i-2]) + cost[i];
+        }
+        return min(F[n-1],F[n-2]);
+    }
+};
