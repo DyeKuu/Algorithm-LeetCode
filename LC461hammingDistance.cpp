f9087d53c8796832f92b90551bb2b6591eebd6ef
DyeKuu <kunhao.zheng@polytechnique.edu>
2020-07-05 12:02:15 +0200

    update 462

---
+++ LC461hammingDistance.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        x = x ^ y;
+        y = 0;
+        while (x != 0) {
+            x = x & (x - 1);
+            y++;
+        }
+        return y;
+    }
+};
